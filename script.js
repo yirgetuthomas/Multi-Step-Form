ef2564503b674815494ae2566f94fe437c788a19
Thomas Yirgetu <tom.dibora@gmail.com>
2023-01-14 20:37:51 -0800

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let form1 = document.getElementById("section1");
 let form2 = document.getElementById("section2");
+let form3 = document.getElementById("section3");
 
 let next1 = document.getElementById('next1');
 let next2 = document.getElementById('next2');
 let back1 = document.getElementById('back1');
+let back2 = document.getElementById('back2');
 
 next1.onclick = function() {
     form1.style.left = '-700px';
@@ -15,5 +17,10 @@ back1.onclick = function() {
 }
 next2.onclick = function() {
     form2.style.left = '-700px';
+    form3.style.left = '40px';
+}
+back2.onclick = function() {
+    form2.style.left = '40px';
+    form3.style.left = '700px';
 }
 
